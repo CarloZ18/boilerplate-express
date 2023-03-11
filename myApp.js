9e7539e9a4b2c4ad1490a7d9769d157f922a9f9a
Your Name <you@example.com>
2023-03-10 23:57:26 -0400

    .

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let app = express();
 
-let absolutePath = __dirname + '/views/index.html'
+
+//Servir un archivo
+let absolutePath1 = __dirname + '/views/index.html'
 app.get('/', (req,res) => {
-  res.sendFile(absolutePath);
+  res.sendFile(absolutePath1);
 })
 
 
+//Servir activos estaticos (hojas de estilo, scripts, imágenes)
+let absolutePath2 = __dirname + '/public'
+app.use('/public',express.static(absolutePath2))
 
 
 
