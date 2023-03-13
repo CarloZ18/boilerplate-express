c1c727d35f46ae6fd12eed1220f1cc958b080554
Your Name <you@example.com>
2023-03-13 19:41:14 -0400

    .

--- myApp.js
+++ myApp.js
@@ -1,56 +1,23 @@
-let express = require('express');
+let express = require("express");
+require("dotenv").config();
 let app = express();
-require('dotenv').config()
-
 
 //Servir un archivo
-let absolutePath1 = __dirname + '/views/index.html'
-app.get('/', (req,res) => {
+let absolutePath1 = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
   res.sendFile(absolutePath1);
-})
-
+});
 
 //Servir activos estaticos (hojas de estilo, scripts, imágenes)
-let absolutePath2 = __dirname + '/public'
-app.use('/public',express.static(absolutePath2))
+let absolutePath2 = __dirname + "/public";
+app.use("/public", express.static(absolutePath2));
 
 //Crear API para servir JSON
-app.get('/json', (req,res) => {
-if(process.env.MESSAGE_STYLE == "uppercase"){
-   res.json({"message": "HELLO JSON"})
-}else{
-  res.json({"message": "Hello json"})
-}
-  })
-  
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
