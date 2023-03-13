97ba1b6f70cd16be2e571053e6ac06f964cab197
Your Name <you@example.com>
2023-03-13 19:28:38 -0400

    .

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 
 //Servir un archivo
@@ -15,7 +16,11 @@ app.use('/public',express.static(absolutePath2))
 
 //Crear API para servir JSON
 app.get('/json', (req,res) => {
-    res.json({"message": "Hello json"});
+if(process.env.MESSAGE_STYLE === "uppercase"){
+   res.json({"message": "HELLO JSON"})
+}else{
+  res.json({"message": "Hello json"})
+}
   })
   
 
