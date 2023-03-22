caac21e7238d30bd91e33747a3b2f1f202814cb2
Your Name <you@example.com>
2023-03-22 16:46:27 -0400

    .

--- myApp.js
+++ myApp.js
@@ -42,7 +42,7 @@ app.get("/:word/echo", (req, res) => {
   res.json({echo:req.params.word});
 });
 
-app.route("/name").get((req,res)=>{
+app.route("/name?first=Mick&last=Jagger").get((req,res)=>{
 res.json({
   name:`${req.query.first} ${req.query.last}`
 })
